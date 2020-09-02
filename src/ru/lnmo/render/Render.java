810adebcfe08330b3f2ca561583904e9a0044e17
azu0nyan <nikgavgav@gmail.com>
2020-09-02 12:35:09 +0300

    some changes

--- src/ru/lnmo/render/Render.java
+++ src/ru/lnmo/render/Render.java
@@ -9,12 +9,12 @@ public class Render {
 //        img.setRGB(500, 300, new Color(255, 0, 200).getRGB());
         for (int i = 0; i < img.getWidth(); i++) {
             for (int j = 0; j < img.getHeight(); j++) {
-//                img.setRGB(i, j, 0xFF00FF );
                 img.setRGB(i, j, new Color(i * j % 256, (i + j) % 256, (i * i + j * j) % 256).getRGB() );
             }
         }
     }
 
+    //Стоит начать с этого
     public static void renderLine(BufferedImage img, int x1, int y1, int x2, int y2, int color){
 
     }
