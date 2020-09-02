b5447d590e315504ee13879d3f014e4830742a6d
azu0nyan <nikgavgav@gmail.com>
2020-09-02 12:31:55 +0300

    Initial commit

---
+++ src/ru/lnmo/render/Main.java
@@ -0,0 +1,63 @@
+package ru.lnmo.render;
+
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Main extends JFrame {
+
+    public static void draw(Graphics2D g) {
+        BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Render.render(img);
+        g.drawImage(img, 0, 0, null);
+    }
+
+    static final int w = 1366;
+    static final int h = 768;
+
+    //магический код позволяющий всему работать, лучше не трогать
+    public static void main(String[] args) throws InterruptedException {
+        Main jf = new Main();
+        jf.setSize(w, h);//размер экрана
+        jf.setUndecorated(false);//показать заголовок окна
+        jf.setTitle("Моя супер программа");
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.createBufferStrategy(2);
+        //в бесконечном цикле рисуем новый кадр
+        while (true) {
+            long frameLength = 1000 / 60; //пытаемся работать из рассчета  60 кадров в секунду
+            long start = System.currentTimeMillis();
+            BufferStrategy bs = jf.getBufferStrategy();
+            Graphics2D g = (Graphics2D) bs.getDrawGraphics();
+            g.clearRect(0, 0, jf.getWidth(), jf.getHeight());
+            draw(g);
+
+            bs.show();
+            g.dispose();
+
+            long end = System.currentTimeMillis();
+            long len = end - start;
+            if (len < frameLength) {
+                Thread.sleep(frameLength - len);
+            }
+        }
+
+    }
+
+    public void keyTyped(KeyEvent e) {
+    }
+
+    //Вызывается когда клавиша отпущена пользователем, обработка события аналогична keyPressed
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
---
+++ src/ru/lnmo/render/Render.java
@@ -0,0 +1,21 @@
+package ru.lnmo.render;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Render {
+
+    public static void render(BufferedImage img){
+//        img.setRGB(500, 300, new Color(255, 0, 200).getRGB());
+        for (int i = 0; i < img.getWidth(); i++) {
+            for (int j = 0; j < img.getHeight(); j++) {
+//                img.setRGB(i, j, 0xFF00FF );
+                img.setRGB(i, j, new Color(i * j % 256, (i + j) % 256, (i * i + j * j) % 256).getRGB() );
+            }
+        }
+    }
+
+    public static void renderLine(BufferedImage img, int x1, int y1, int x2, int y2, int color){
+
+    }
+}
